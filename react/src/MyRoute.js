d085448f891ea3a959cedeeaae82b3f00400cfdd
chalermsak <gambitmis@gmail.com>
2022-02-08 12:09:59 +0000

    start lesson 35

--- react/src/MyRoute.js
+++ react/src/MyRoute.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 import App from "./App"
 import FormComponent from "./components/FormComponent"
+import SingleComponent from "./components/SingleComponent"
 
 const MyRoute=()=>{
     return(
@@ -8,6 +9,7 @@ const MyRoute=()=>{
             <Switch>
                 <Route path="/" exact component={App}></Route>
                 <Route path="/create" exact component={FormComponent}></Route>
+                <Route path="/blog/:slug" exact component={SingleComponent}></Route>
             </Switch>
         </BrowserRouter>
     )
---
+++ react/src/components/SingleComponent.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useState,useEffect } from "react"
+import NavBarComponent from "./NavbarComponent";
+
+const SingleComponent=(props)=>{
+    const [blog,setBlog] = useState('')
+    useEffect(()=>{
+        axios
+        .get(`${process.env.REACT_APP_API}/blog/${props.match.params.slug}`)
+        .then(response=>{
+            setBlog(response.data)
+        })
+        .catch(err=>alert(err))
+    },[])
+    return(
+        <div className="container p-5">
+            <NavBarComponent/>
+            <h1>{blog.title}</h1>
+            <p>{blog.content}</p>
+            <p className="text=muted">Author: {blog.author} , Published : {new Date(blog.createdAt).toLocaleString()} , Updated: {new Date(blog.updatedAt).toLocaleString()}</p>
+        </div>
+    )
+}
+export default SingleComponent;
